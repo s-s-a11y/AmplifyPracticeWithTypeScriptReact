f083ef498b24aa5baf943eff5a22b253bc3abe49
Sshiigai <itkonwa2026040605@gmail.com>
2026-08-17 12:47:32 +0900

    add user handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+"use client";
 import { UserRegist } from "./components/UserRegist";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Login } from "./components/Login";
 import { Home } from "./components/Home";
 
 function App() {
   const [currentView, setCurrentView] = useState<string>("home");
+  const [loginUserName, setLoginUserName] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loginUser = sessionStorage.getItem("loginUserName");
+    setLoginUserName(loginUser);
+  });
+
   const renderView = () => {
     switch (currentView) {
       case "userRegist":
@@ -16,16 +24,31 @@ function App() {
         return <Home />;
     }
   };
+
+  const logout = () => {
+    sessionStorage.clear();
+    setLoginUserName(null);
+    setCurrentView("login");
+  };
   return (
     <>
       <section id="center">
         <nav>
-          <button onClick={() => setCurrentView("login")}>ログイン</button> |{" "}
-          <button onClick={() => setCurrentView("userRegist")}>
-            新規ユーザー登録
-          </button>
-          {" | "}
-          <button onClick={() => setCurrentView("home")}>HOME</button>{" "}
+          {!loginUserName && (
+            <div>
+              <button onClick={() => setCurrentView("login")}>ログイン</button>{" "}
+              |{" "}
+              <button onClick={() => setCurrentView("userRegist")}>
+                新規ユーザー登録
+              </button>
+            </div>
+          )}
+          {loginUserName && (
+            <div>
+              <button onClick={() => setCurrentView("home")}>HOME</button> |{" "}
+              <button onClick={logout}>ログアウト</button>
+            </div>
+          )}
         </nav>
       </section>
       <div className="content-area">{renderView()}</div>
